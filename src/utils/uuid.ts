bf3ec3a74345a56fe1af389e19d332c78e96e5c0
Frikilinux <frikilinux@gmail.com>
2023-08-02 20:37:14 -0300

    Add node version

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,9 +1,9 @@
-const uuid = () => {
+const uuid = (): string => {
   const hexDigits = 'abcdef0123456789'
-  const uuid = Array(36).fill(null)
+  const uuid: string[] = Array(36).fill(null)
 
   return uuid
-    .map((e, i) => {
+    .map((_, i) => {
       return i === 8 || i === 13 || i === 18 || i === 23
         ? '-'
         : hexDigits[Math.floor(Math.random() * hexDigits.length)]
